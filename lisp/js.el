c8cfcc5cf1b61799cc34041031f644fcec9db709
Lee Houghton <lee@theconnectgroup.net>
2014-11-30 14:10:39 +0000

    Fix js2-mode not loading for .js files.

--- lisp/js.el
+++ lisp/js.el
@@ -1,7 +1,10 @@
 (require 'js2-mode)
 
-(setq js2-global-externs '(app $ jsSHA dust moment))
+(setq js2-global-externs '(app $ jsSHA dust moment async))
 (setq js2-include-node-externs t)
 (setq js2-indent-switch-body t)
 (setq js2-missing-semi-one-line-override t)
 (setq js2-strict-missing-semi-warning nil)
+
+(with-eval-after-load "js2-mode"
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode)))
