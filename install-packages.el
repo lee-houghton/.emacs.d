5a272b18a36e3d1bd0fe9ef5e42c9cef62d8def2
Lee Houghton <lee@theconnectgroup.net>
2014-12-06 14:14:29 +0000

    Add csharp-mode

--- install-packages.el
+++ install-packages.el
@@ -11,5 +11,6 @@
 	     gruvbox-theme haskell-mode how-many-lines-in-project js2-mode
 	     keyfreq lorem-ipsum magit mark-multiple markdown-mode
 	     multiple-cursors nyan-mode nyan-prompt pgdevenv rainbow-mode
-	     scss-mode unbound web-mode zencoding-mode undo-tree dos))
+	     scss-mode unbound web-mode zencoding-mode undo-tree dos
+         csharp-mode))
   (package-install p))
