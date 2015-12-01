def174c3952551d8290f2d550616bc6e1abdfccb
Lee Houghton <lee@theconnectgroup.net>
2015-12-01 13:27:47 +0000

    Commit a load of changes I have locally :+1:
     * Added coffee.el (C-c r to compile region)
     * Turn off lock files
     * Use ido everywhere with flex matching.
     * Fix error when declining to donate.
     * Turn off default snippets (annoying)

--- .mc-lists.el
+++ .mc-lists.el
@@ -3,13 +3,19 @@
 
 (setq mc/cmds-to-run-for-all
       '(
+        beginning-of-buffer
         coffee-dedent-line-backspace
+        downcase-region
         electric-pair-delete-pair
         indent-for-tab-command
+        kill-region
         zencoding-expand-line
         ))
 
 (setq mc/cmds-to-run-once
       '(
+        inline-string-rectangle
         js2-mode-show-node
+        tab-to-tab-stop
+        yas-expand
         ))
--- init.el
+++ init.el
@@ -4,6 +4,8 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 ;; Much nicer than Helm!
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
 (ido-mode t)
 
 ;; Turn off annoying things.
@@ -12,6 +14,10 @@
 (setq indent-tabs-mode nil)
 (setq-default indent-tabs-mode nil)
 (setq inhibit-startup-screen t)
+(setq create-lockfiles nil)
+
+;; Turn off some annoying default snippets
+(setq yas-snippet-dirs '("~/.emacs.d/snippets"))
 
 ;; Turn on some useful things.
 (electric-indent-mode t)
@@ -23,6 +29,7 @@
 (delete-selection-mode t)
 (toggle-save-place-globally t)
 (put 'dired-find-alternate-file 'disabled nil)
+(put 'downcase-region 'disabled nil)
 
 ;; Default settings.
 (setq default-tab-width 4)
@@ -41,6 +48,23 @@
 
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
-(dolist (f '("css" "editing" "er" "haskell" "html" "js" "keys" "markdown" "mc" "sublime" "batch" "sequel" "yas" "brogrammer-theme"))
+(dolist (f '("coffee" "css" "editing" "er" "haskell" "html" "js2-stuff" "keys" "markdown" "mc" "sublime" "batch" "sequel" "yas" "brogrammer-theme"))
   (load-file (concat "~/.emacs.d/lisp/" f ".el")))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(custom-safe-themes
+   (quote
+    ("806d8c827b214f5f60348114bd27c6dcb5d19047f7ac482ad61e8077a6c5ea60" default)))
+ '(sql-connection-alist
+   (quote
+    (("portal"
+      (sql-product
+       (quote postgres))
+      (sql-user "admin")
+      (sql-database "portal")
+      (sql-server "ctgtesting-serv"))))))
